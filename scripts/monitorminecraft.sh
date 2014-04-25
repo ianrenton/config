15f4fd81c4f02b0d5a66c2716f011a0b0a6f8c80
Ian Renton <ian@ianrenton.com>
2014-04-25 20:25:10 +0100

    Updated for craftbukkit 1.7.2

--- scripts/monitorminecraft.sh
+++ scripts/monitorminecraft.sh
@@ -2,10 +2,11 @@
 
 # Monitors a CraftBukkit Minecraft server for logins, and sends an email when someone logs in
 
-tail -f /home/ian/bin/craftbukkit/server.log | 
+tail -f /home/ian/bin/craftbukkit/logs/latest.log | 
   while read line; do
     if [[ $line == *logged* ]] || [[ $line == *left* ]]
     then
+      echo "Emailing: $line"
       echo -e "$line" | mail -s "Minecraft server notification" "minecraft@ianrenton.com"
     fi
   done
