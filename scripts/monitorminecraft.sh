7332a3e1434a910804488e79d5fe87d7353832e5
Ian Renton <ian@ianrenton.com>
2014-04-28 07:08:05 +0100

    CraftBukkit's log rotation seems to kill off the tail, so trying this ugly hack.

--- scripts/monitorminecraft.sh
+++ scripts/monitorminecraft.sh
@@ -2,11 +2,14 @@
 
 # Monitors a CraftBukkit Minecraft server for logins, and sends an email when someone logs in
 
-tail -f /home/ian/bin/craftbukkit/logs/latest.log | 
-  while read line; do
-    if [[ $line == *logged* ]] || [[ $line == *left* ]]
-    then
-      echo "Emailing: $line"
-      echo -e "$line" | mail -s "Minecraft server notification" "minecraft@ianrenton.com"
-    fi
-  done
+while :
+do
+  tail -f /home/ian/bin/craftbukkit/logs/latest.log | 
+    while read line; do
+      if [[ $line == *logged* ]] || [[ $line == *left* ]]
+      then
+        echo "Emailing: $line"
+        echo -e "$line" | mail -s "Minecraft server notification" "minecraft@ianrenton.com"
+      fi
+    done
+done
